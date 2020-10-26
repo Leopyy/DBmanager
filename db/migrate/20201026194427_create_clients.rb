60f551d019ea64294f249e64e44c2420cc3c2b7d
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-10-26 16:58:52 -0300

    create clients model and controller

---
+++ db/migrate/20201026194427_create_clients.rb
@@ -0,0 +1,12 @@
+class CreateClients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :address
+      t.string :telephone
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
